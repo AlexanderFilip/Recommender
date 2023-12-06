79913c6b1c1004330b8c2d66e25509f744f32a13
Alexf <filip.alexandru44@yahoo.com>
2023-12-06 15:52:45 +0000

    add scratch project

---
+++ Recommender/src/test/java/com/crypto/Recommender/RecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.Recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
